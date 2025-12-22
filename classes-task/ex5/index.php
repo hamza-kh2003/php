c2a178cebd27d4d8f5f91d2ed5489bc0125edef1
Hamza Alkhader <hamzaalkhader25@gmail.com>
2025-12-23 00:08:20 +0300

    update some tasks

--- classes-task/ex5/index.php
+++ classes-task/ex5/index.php
@@ -1,12 +1,16 @@
 <?php
 
+class DateDifference {
+    public function getDifference($date1, $date2) {
+        $datetime1 = new DateTime($date1);
+        $datetime2 = new DateTime($date2);
+        $interval = $datetime1->diff($datetime2);
 
-$date1 = new DateTime("1981-11-03");
-$date2 = new DateTime("2013-09-04");
+        return "Difference : {$interval->y} years, {$interval->m} months, {$interval->d} days";
+    }
+}
 
-$diff=$date1->diff($date2);
+$object = new DateDifference();
 
-echo "Difference : $diff->y years, $diff->m months, $diff->d days";
-
-
+echo $object->getDifference("1981-11-03", "2013-09-04");
 ?>
--- classes-task/ex6/index.php
+++ classes-task/ex6/index.php
@@ -1,10 +1,18 @@
 <?php
 
-$dateString = '12-08-2004';
+class DateFormatter {
+    public function convertStringToDate($dateString) {
+        // Input '12-08-2004' is interpreted as m-d-Y (December 8, 2004)
+        $date = DateTime::createFromFormat('m-d-Y', $dateString);
+        if ($date) {
+            return $date->format('Y-m-d');
+        } else {
+            return "Invalid date format";
+        }
+    }
+}
 
-$date = DateTime::createFromFormat('d-m-Y', $dateString);
+$object=new DateFormatter();
 
-echo $date->format('Y-m-d');
-
-
+echo $object->convertStringToDate("12-08-2004");
 ?>
